1029ff57dd81aac3849a919bdc31e93232d6388a
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-05-16 15:26:51 -0700

    deployment

--- src/Components/Body/BodyData/BodyData.js
+++ src/Components/Body/BodyData/BodyData.js
@@ -1,64 +1,128 @@
-import React, {useState, useEffect} from "react";
-// import ReactDOM from "react-dom";
-// import "../../../App.css";
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../../App.css";
 import axios from "axios";
-import NasaCard from "./BodyDataButton"
+import moment from "moment";
 import styled from "styled-components";
 
-
-const Container = styled.div `
-  margin: auto;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
   text-align: center;
-  background:#778899;
-  padding:20px;
+  background: #263238;
+  padding: 20px;
 `;
 
 const DateStyle = styled.h2`
-  color:BEIGE;
+  color: BEIGE;
 `;
 
+const Header = styled.h1`
+  color: white;
+  margin: auto;
+`;
+const Header2 = styled.h2`
+  color: white;
+  margin: auto;
+`;
 const Description = styled.p`
-  width:80%;
-  margin:auto;
-  color:black;
+  margin: auto;
+  color: white;
+  // width: 95%;
+  padding: 5px;
+  line-height: 1.5;
+  word-space: 10px;
+  text-align: left;
 `;
 const DateButton = styled.button`
-  padding:50px;
+  background: #ab003c;
+  padding: 10px;
+  margin: auto;
+  font-size: 15px;
+  color: white;
+  border: 1px solid white;
+  border-radius: 5px;
+`;
+
+const Body = styled.div`
+  border: 1px solid grey;
+  border-radius: 15px;
+  background: #ab003c;
+  margin: auto;
+  margin-top: 2%;
+  width: 80%;
+`;
+
+const Image = styled.div`
+  margin: auto;
+`;
+const Date = styled.div`
+  margin: auto;
+  margin-bottom: 2%;
+  margin-top: 2%;
+`;
+
+const Cal = styled.div`
+  padding: 10px;
+  margin: auto;
+  color: white;
+  background: #ab003c;
+  font-size: 15px;
 `;
 
 export default function NasaPicture() {
-  const [nasa, setNasa ] = useState([]);
-  const [date, setDate] = useState(new Date());
+  const [nasa, setNasa] = useState([]);
+  const [selectedDate, setSelectedDate] = useState();
 
-
-  // let dd = date.getDate();
-  // let mm = date.getMonth()+1;
-  // let yyyy = date.getFullYear();
-
-  
+  // const handleChange = (e) => {
+  //   // e.preventDefault();
+  //   setSelectedDate(selectedDate);
+  //   console.log("date", selectedDate);
+  // };
   useEffect(() => {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=olBYwidSzG1KBoxDgoUcPMhX2R0QfW7aMxVkyl7X&date=2018-10-28`)
-        .then(response => {
-          const nasa = response.data;
-          console.log(nasa);
-          setNasa(nasa);
-        })
-        .catch(error => {
-          console.log("Sorry, no images being displayed");
-        });
-  }, []); 
-
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=olBYwidSzG1KBoxDgoUcPMhX2R0QfW7aMxVkyl7X&date=${moment(
+          selectedDate
+        ).format("YYYY-MM-DD")}`
+      )
+      .then((response) => {
+        const nasa = response.data;
+        console.log(nasa);
+        setNasa(nasa);
+      })
+      .catch((error) => {
+        console.log("Sorry, no images being displayed");
+      });
+  }, [selectedDate]);
 
   return (
     <Container>
-      
-         <DateStyle>{nasa.date}</DateStyle>
-         <img className = "img-source" src = {nasa.url} />
-         <Description> {nasa.explanation}</Description>
-         <DateButton onClick={() => setDate({})}>Random Date</DateButton>
-      
-
+      <Header>Nasa-Photo of the Day</Header>
+      <Date>
+        <Cal>
+          Pick a date:{" "}
+          <DatePicker
+            selected={selectedDate}
+            onChange={(date) => setSelectedDate(date)}
+            dateFormat="yyyy/MM/dd"
+            // maxDate={new Date()}
+            showYearDropdown
+            scrollableMonthYearDropdown
+          />
+        </Cal>
+        {/* <DateButton onClick={handleChange}>Pick Date!</DateButton> */}
+      </Date>
+      <DateStyle>{nasa.date}</DateStyle>
+      <Image>
+        <img className="img-source" src={nasa.url} />
+      </Image>
+      <Body>
+        <Header2>{nasa.title}</Header2>{" "}
+        <Description> {nasa.explanation}</Description>
+      </Body>
     </Container>
   );
 }
